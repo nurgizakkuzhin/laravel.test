9a5d470ace150a3198ec6b146a05e44945daf0e0
Nurgiz <akkuzhin.n@gmail.com>
2023-09-23 21:56:59 +0300

    Пагинация с фильтром

--- resources/views/worker/index.blade.php
+++ resources/views/worker/index.blade.php
@@ -58,7 +58,7 @@
             <hr>
         @endforeach
         <div class="my-nav">
-            {{ $workers->links() }}
+            {{ $workers->withQueryString()->links() }}
         </div>
     </div>
 </body>
