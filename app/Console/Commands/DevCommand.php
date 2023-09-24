316587d15ca72d20ffab0e6814d090f2ca4a4d74
Nurgiz <akkuzhin.n@gmail.com>
2023-09-24 11:38:46 +0300

    Создания своих консольных команд php artisan make:command DevCommand(Название команды)

---
+++ app/Console/Commands/DevCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class DevCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'develop';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Some develops';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        dd(111111111);
+    }
+}
